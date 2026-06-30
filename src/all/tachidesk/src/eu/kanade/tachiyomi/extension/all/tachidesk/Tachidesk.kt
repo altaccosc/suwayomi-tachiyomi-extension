e8e3bf7cc17b2a608276341fe97761374d268bfc
altaccosc <97206249+altaccosc@users.noreply.github.com>
2026-06-30 19:31:03 +0200

    Use realUrl instead of baseUrl + url

--- src/all/tachidesk/src/eu/kanade/tachiyomi/extension/all/tachidesk/Tachidesk.kt
+++ src/all/tachidesk/src/eu/kanade/tachiyomi/extension/all/tachidesk/Tachidesk.kt
@@ -896,7 +896,7 @@ class Tachidesk : ConfigurableSource, UnmeteredSource, HttpSource() {
     private fun MangaFragment.toSManga() = SManga.create().also {
         var desc = description
         if (getPrefMentionSourceInDescription() && source != null) {
-            val append = "\n\n**Source:** [${source.displayName}](${source.baseUrl}$url)"
+            val append = "\n\n**Source:** [${source.displayName}]($realUrl)"
             if (desc == null) {
                 desc = append
             } else {
